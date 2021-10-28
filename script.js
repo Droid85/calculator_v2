7e6a9afd5b7e5763cbe9e796b6d51b1e8ec9cdf8
Andrii <droid85@yahoo.com>
2021-10-28 19:51:51 +0300

    compleat script.js with class Calculator

--- script.js
+++ script.js
@@ -1,16 +1,26 @@
-const digitsBtns = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
-const calcOperands = ['+', '-', '/', '*'];
-const calcTag = 'button';
 let operatorFirst = [];
 let operatorSecond = [];
 let operand = '';
-let resultat = 0;
-let num1 = 0;
-let num2 = 0;
+let resultat = null;
+let num1 = null;
+let num2 = null;
 
 function Calculator(numberOne, numberTwo) {
     this.numberOne = numberOne;
     this.numberTwo = numberTwo;
+    this.digitsBtns = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
+    this.calcOperands = ['+', '-', '/', '*'];
+    this.calcTag = 'button';
+    this.btnDigitClass = 'digit-bts';
+    this.btnOperandClass = 'operands-bts';
+
+    this.screenEl = document.querySelector('#screen');
+    this.clearBtnEl = document.querySelector('#clr');
+    this.digitsBtnEl = document.querySelector('#digits');
+    this.operandsBtnEl = document.querySelector('#operands');
+    this.resultBtnEl = document.querySelector('#resultBtn');
+
+
     this.sum = function() {
         return this.numberOne + this.numberTwo;
     }
@@ -25,19 +35,15 @@ function Calculator(numberOne, numberTwo) {
     }
 }
 
-const screenEl = document.querySelector('#screen');
-const clearBtnEl = document.querySelector('#clr');
-const digitsBtnEl = document.querySelector('#digits');
-const operandsBtnEl = document.querySelector('#operands');
-const resultBtnEl = document.querySelector('#resultBtn');
+calc = new Calculator();
 
-digitsBtnEl.addEventListener('click', onClickDigitsBtn);
+calc.digitsBtnEl.addEventListener('click', onClickDigitsBtn);
 
-operandsBtnEl.addEventListener('click', onClickOperandsBtn);
+calc.operandsBtnEl.addEventListener('click', onClickOperandsBtn);
 
-clearBtnEl.addEventListener('click', onClearScreen);
+calc.clearBtnEl.addEventListener('click', onClearScreen);
 
-resultBtnEl.addEventListener('click', onResultBtn);
+calc.resultBtnEl.addEventListener('click', onResultBtn);
 
 function createElements(conteiner, tag, content, className) {
     content.forEach(item => {
@@ -50,16 +56,16 @@ function createElements(conteiner, tag, content, className) {
 }
 
 function onClickDigitsBtn(e) {
-    const elements = digitsBtnEl.children;
+    const elements = calc.digitsBtnEl.children;
     for (let i = 0; i < elements.length; i++) {
         if (e.target.id && elements[i].id && elements[i].id === e.target.id) {
             if (!operand) {
                 operatorFirst.push(+elements[i].textContent);
-                screenEl.textContent = operatorFirst.join('');
+                calc.screenEl.textContent = operatorFirst.join('');
                 num1 = +operatorFirst.join('');
             } else {
                 operatorSecond.push(+elements[i].textContent);
-                screenEl.textContent = operatorFirst.join('') + operand + operatorSecond.join('');
+                calc.screenEl.textContent = operatorFirst.join('') + operand + operatorSecond.join('');
                 num2 = +operatorSecond.join('');
             }
         }
@@ -67,37 +73,37 @@ function onClickDigitsBtn(e) {
 }
 
 function onClickOperandsBtn(e) {
-    const elements = operandsBtnEl.children;
+    const elements = calc.operandsBtnEl.children;
     for (let i = 0; i < elements.length; i++) {
         if (e.target.id && elements[i].id && elements[i].id === e.target.id) {
             if (operand === '') {
             operand = elements[i].textContent;
-            screenEl.textContent = operatorFirst.join('') + operand;
+            calc.screenEl.textContent = operatorFirst.join('') + operand;
             }
         }
     }
 }
 
 function onClearScreen() {
-    screenEl.textContent = '';
+    calc.screenEl.textContent = '';
     operatorFirst = [];
     operatorSecond = [];
     operand = '';
 }
 
-createElements(digitsBtnEl, calcTag, digitsBtns, 'digit-bts');
+createElements(calc.digitsBtnEl, calc.calcTag, calc.digitsBtns, calc.btnDigitClass);
 
-createElements(operandsBtnEl, calcTag, calcOperands, 'operands-bts');
+createElements(calc.operandsBtnEl, calc.calcTag, calc.calcOperands, calc.btnOperandClass);
 
 function onResultBtn() {
     resultat = new Calculator(num1, num2);
     if (operand === '+') {
-        screenEl.textContent = resultat.sum();
+        calc.screenEl.textContent = resultat.sum();
     } else if (operand === '-') {
-        screenEl.textContent = resultat.diff();
+        calc.screenEl.textContent = resultat.diff();
     } else if (operand === '*') {
-        screenEl.textContent = resultat.mult();
+        calc.screenEl.textContent = resultat.mult();
     } else if (operand === '/') {
-        screenEl.textContent = resultat.divide();
+        calc.screenEl.textContent = resultat.divide();
     }
 }
